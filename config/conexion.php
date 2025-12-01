3904b141a324c001f8ddea934532efc523bc00b9
alancontrerasss <alanfernando081004@gmail.com>
2025-12-01 09:20:48 -0600

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -1,13 +1,15 @@
 <?php
 // config/conexion.php
+
 date_default_timezone_set('America/Mexico_City');
 
-$host = "localhost";
-$usuario = "root";
-$clave = "alan2004";
-$bd = "oficina";
+$host    = getenv('MYSQLHOST') ?: '127.0.0.1';
+$usuario = getenv('MYSQLUSER') ?: 'root';
+$clave   = getenv('MYSQLPASSWORD') ?: '';
+$bd      = getenv('MYSQLDATABASE') ?: 'oficina';
+$port    = getenv('MYSQLPORT') ?: 3306;
 
-$conexion = new mysqli($host, $usuario, $clave, $bd);
+$conexion = new mysqli($host, $usuario, $clave, $bd, (int)$port);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
